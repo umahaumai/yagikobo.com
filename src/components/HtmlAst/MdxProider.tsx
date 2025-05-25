ac3bd8e46d4930eeedc1bb65e31bd8160836e0a0
umahaumai <58021946+umahaumai@users.noreply.github.com>
2025-05-25 18:21:12 +0900

    #7 Markdown の css を調整して読みやすくした (#13)

--- src/components/HtmlAst/MdxProider.tsx
+++ src/components/HtmlAst/MdxProider.tsx
@@ -4,20 +4,29 @@ import { Divider, Link, Typography } from '@mui/material';
 import React from 'react';
 
 export const components = {
-  h1: (props: Readonly<Props>) => <Typography variant="h1" {...props} />,
-  h2: (props: Readonly<Props>) => <Typography variant="h2" {...props} />,
-  h3: (props: Readonly<Props>) => <Typography variant="h3" {...props} />,
+  h1: (props: Readonly<Props>) => (
+    <>
+      <Typography variant="h1" sx={{ fontSize: '2rem', fontWeight: 'bold' }} {...props} />
+      <Divider />
+    </>
+  ),
+  h2: (props: Readonly<Props>) => (
+    <Typography variant="h2" sx={{ fontSize: '1.5rem', fontWeight: 'bold', mt: 2 }} {...props} />
+  ),
+  h3: (props: Readonly<Props>) => <Typography variant="h3" sx={{ mt: 2 }} {...props} />,
   h4: (props: Readonly<Props>) => <Typography variant="h4" {...props} />,
   h5: (props: Readonly<Props>) => <Typography variant="h5" {...props} />,
   h6: (props: Readonly<Props>) => <Typography variant="h6" {...props} />,
   thematicBreak: (props: Readonly<Props>) => <Divider {...props} />,
   hr: (props: Readonly<Props>) => <Divider {...props} />,
   a: (props: Readonly<Props>) => <Link {...props} />,
-  p: (props: Readonly<Props>) => <Typography variant="body1" {...props} />,
+  p: (props: Readonly<Props>) => <Typography variant="body1" sx={{ mt: 2, whiteSpace: 'pre-line' }} {...props} />,
   strong: (props: Readonly<Props>) => <Typography variant="body1" component="strong" {...props} />,
   ul: (props: Readonly<Props>) => <Typography variant="body1" component="ul" {...props} />,
   ol: (props: Readonly<Props>) => <Typography variant="body1" component="ol" {...props} />,
-  li: (props: Readonly<Props>) => <Typography variant="body1" component="li" {...props} />,
+  li: (props: Readonly<Props>) => (
+    <Typography variant="body1" component="li" sx={{ whiteSpace: 'pre-line' }} {...props} />
+  ),
   blockquote: (props: Readonly<Props>) => <Typography variant="body1" component="blockquote" {...props} />,
   code: (props: Readonly<Props>) => <Typography variant="body1" component="code" {...props} />,
   inlineCode: (props: Readonly<Props>) => <Typography variant="body1" component="code" {...props} />,
--- src/templates/mdx.tsx
+++ src/templates/mdx.tsx
@@ -1,6 +1,7 @@
 import { MdxWrapper } from '@/components/HtmlAst/MdxProider';
 import SubHeader from '@/components/SubHeader';
-import { Box } from '@mui/material';
+import { Box, Divider, Typography } from '@mui/material';
+import dayjs from 'dayjs';
 import type { PageProps } from 'gatsby';
 import React, { useEffect } from 'react';
 
@@ -10,7 +11,7 @@ export const Head = ({ pageContext }: InfoProps) => {
   return <title>{pageContext?.frontmatter?.title} - 株式会社やぎ工房</title>;
 };
 
-export default function Info({ children }: InfoProps) {
+export default function Info({ children, pageContext }: InfoProps) {
   // 画面表示時に頁のトップにスクロールする
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -19,6 +20,10 @@ export default function Info({ children }: InfoProps) {
     <Box>
       <SubHeader />
       <MdxWrapper>{children}</MdxWrapper>
+      <Divider sx={{ my: 2 }} />
+      <Typography variant="body2" sx={{ textAlign: 'right' }}>
+        更新日: {dayjs(pageContext.frontmatter?.updatedAt).format('YYYY-MM-DD')}
+      </Typography>
     </Box>
   );
 }
